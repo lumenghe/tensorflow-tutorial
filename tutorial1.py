403a293c3f40c887c3c41096d13829351d8f2cb6
lumenghe <lumenghe@gmail.com>
2018-02-26 10:28:44 +0000

    local assignements

--- tutorial1.py
+++ tutorial1.py
@@ -134,3 +134,9 @@ def maybe_pickle(data_folders, min_num_images_per_class, force=False):
         print('Unable to save data to', set_filename, ':', e)
 
   return dataset_names
+
+train_datasets = maybe_pickle(train_folders, 45000)
+test_datasets = maybe_pickle(test_folders, 1800)
+
+
+
