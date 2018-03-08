ab1931a57da4512998476b437f8ef73ad2820f21
lumenghe <lumenghe@gmail.com>
2018-03-08 11:34:28 +0000

    adding top level assignements

--- tutorial1.py
+++ tutorial1.py
@@ -206,3 +206,7 @@ def randomize(dataset, labels):
   shuffled_dataset = dataset[permutation,:,:]
   shuffled_labels = labels[permutation]
   return shuffled_dataset, shuffled_labels
+train_dataset, train_labels = randomize(train_dataset, train_labels)
+test_dataset, test_labels = randomize(test_dataset, test_labels)
+valid_dataset, valid_labels = randomize(valid_dataset, valid_labels)
+
