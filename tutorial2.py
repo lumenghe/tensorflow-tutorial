c4a340b0f94bcd517b252f46d74c97565bddbf44
lumenghe <lumenghe@gmail.com>
2018-05-18 15:01:07 +0100

    define function accuracy

--- tutorial2.py
+++ tutorial2.py
@@ -84,3 +84,7 @@ with graph.as_default():
 
 
 num_steps = 801
+
+def accuracy(predictions, labels):
+  return (100.0 * np.sum(np.argmax(predictions, 1) == np.argmax(labels, 1))
+          / predictions.shape[0])
