db3780516a9015ad41f3d376cf324bb82f05f4f8
lumenghe <lumenghe@gmail.com>
2018-05-26 12:03:00 +0100

    new function 'reformat'

--- tutorial3.py
+++ tutorial3.py
@@ -26,3 +26,9 @@ with open(pickle_file, 'rb') as f:
 
 image_size = 28
 num_labels = 10
+
+def reformat(dataset, labels):
+  dataset = dataset.reshape((-1, image_size * image_size)).astype(np.float32)
+  # Map 1 to [0.0, 1.0, 0.0 ...], 2 to [0.0, 0.0, 1.0 ...]
+  labels = (np.arange(num_labels) == labels[:,None]).astype(np.float32)
+  return dataset, labels
