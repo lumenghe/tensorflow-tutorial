cab43e28acc267e1bde467ef7444b621b5e0e5bc
lumenghe <lumenghe@gmail.com>
2018-05-29 14:03:07 +0100

    workflow modification

--- tutorial2.py
+++ tutorial2.py
@@ -199,3 +199,28 @@ with graph.as_default():
     test_prediction = tf.nn.softmax(tf.matmul(tf.nn.relu( tf.matmul(tf_test_dataset, weight_1) + biases_1), weight_2) + biases_2)
 
 num_steps = 3001
+
+with tf.Session(graph=graph) as session:
+    tf.global_variables_initializer().run()
+    print("Initialized")
+    for step in range(num_steps):
+        offset = (step * batch_size) % (train_labels.shape[0] - batch_size)
+
+        batch_data = train_dataset[offset: (offset + batch_size), :]
+        batch_labels = train_labels[offset: (offset + batch_size), :]
+
+        feed_dict = {tf_train_dataset: batch_data, tf_train_labels: batch_labels}
+
+        _, l, predictions = session.run([optimizer, loss, train_prediction], feed_dict=feed_dict)
+
+        if (step%500==0):
+            print ("Minibatch loss at step", step, ":", l)
+            print ("Minibatch accuracy: %.1f%%" % accuracy(predictions, batch_labels))
+            print ("Validation accuracy: %.1f%%" % accuracy(valid_prediction.eval(), valid_labels))
+
+    print ("Test accuracy: %.1f%%" % accuracy(test_prediction.eval(), test_labels))
+
+
+
+
+
