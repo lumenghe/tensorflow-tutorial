e1b26315c5391c1098b7345e1e47248c78a672a1
lumenghe <lumenghe@gmail.com>
2018-07-12 09:47:18 +0100

    updating environement

--- tutorial5.py
+++ tutorial5.py
@@ -123,3 +123,45 @@ valid_examples = np.array(random.sample(range(valid_window), valid_size))
 num_sampled = 64 # Number of negative examples to sample.
 
 graph = tf.Graph()
+
+with graph.as_default(), tf.device('/cpu:0'):
+
+  # Input data.
+  train_dataset = tf.placeholder(tf.int32, shape=[batch_size])
+  train_labels = tf.placeholder(tf.int32, shape=[batch_size, 1])
+  valid_dataset = tf.constant(valid_examples, dtype=tf.int32)
+
+  # Variables.
+  embeddings = tf.Variable(
+    tf.random_uniform([vocabulary_size, embedding_size], -1.0, 1.0))
+  softmax_weights = tf.Variable(
+    tf.truncated_normal([vocabulary_size, embedding_size],
+                         stddev=1.0 / math.sqrt(embedding_size)))
+  softmax_biases = tf.Variable(tf.zeros([vocabulary_size]))
+
+  # Model.
+  # Look up embeddings for inputs.
+  embed = tf.nn.embedding_lookup(embeddings, train_dataset)
+  # Compute the softmax loss, using a sample of the negative labels each time.
+  loss = tf.reduce_mean(
+    tf.nn.sampled_softmax_loss(weights=softmax_weights, biases=softmax_biases, inputs=embed,
+                               labels=train_labels, num_sampled=num_sampled, num_classes=vocabulary_size))
+
+  # Optimizer.
+  # Note: The optimizer will optimize the softmax_weights AND the embeddings.
+  # This is because the embeddings are defined as a variable quantity and the
+  # optimizer's `minimize` method will by default modify all variable quantities
+  # that contribute to the tensor it is passed.
+  # See docs on `tf.train.Optimizer.minimize()` for more details.
+  optimizer = tf.train.AdagradOptimizer(1.0).minimize(loss)
+
+  # Compute the similarity between minibatch examples and all embeddings.
+  # We use the cosine distance:
+  norm = tf.sqrt(tf.reduce_sum(tf.square(embeddings), 1, keep_dims=True))
+  normalized_embeddings = embeddings / norm
+  valid_embeddings = tf.nn.embedding_lookup(
+    normalized_embeddings, valid_dataset)
+  similarity = tf.matmul(valid_embeddings, tf.transpose(normalized_embeddings))
+
+
+
