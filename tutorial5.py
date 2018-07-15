1f7ef97764343848941b4f659e64e55185ced7b9
lumenghe <lumenghe@gmail.com>
2018-07-15 12:32:47 +0100

    local assignements

--- tutorial5.py
+++ tutorial5.py
@@ -198,3 +198,8 @@ with tf.Session(graph=graph) as session:
         print(log)
   final_embeddings = normalized_embeddings.eval()
 
+
+num_points = 400
+
+tsne = TSNE(perplexity=30, n_components=2, init='pca', n_iter=5000)
+two_d_embeddings = tsne.fit_transform(final_embeddings[1:num_points+1, :])
